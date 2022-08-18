03024e4ef0c80503634bbf676cc4f342d7263755
kieuhuy <kieuhuy2000@gmail.com>
2022-08-18 10:21:40 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import './App.css';
 import React from "react";
 function App() {
   return (
-    <div>
-      hello
+        <div class="Menu">
+        <h2>longo</h2>
+        <h6>AUTOPLAZA</h6>
+        <p><img src={'/iconDashboard.png'} alt={'icon'}/>&nbsp;
+        Dashboard</p>
+        <p1>Car Pricing</p1><br/><br/>
+        <p><img src={'/PlusCircle.png'} alt={'icon'}/>&nbsp;Request</p>
+        <p><img src={'/Union.png'} alt={'icon'}/>&nbsp;Allocation</p>
+        <p><img src={'/Pricing.png'} alt={'icon'}/>&nbsp;Pricing</p><br/><br/>
+
+        <p1>Managment</p1>
+        <p><img src={'/User.png'} alt={'icon'}/>&nbsp; User </p>
+        <p><img src={'/Setting.png'} alt={'icon'}/>&nbsp; Setting</p>
+
+
     </div>
   );
 }
