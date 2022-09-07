284dd8b45ac12ed1b383e397bb87dc7b696e2bfc
kieuhuy <kieuhuy2000@gmail.com>
2022-09-07 17:34:11 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -8,18 +8,15 @@ function App() {
                 <div class="Menu" style={{width: 236}}>
                       <h2>longo</h2>
                       <h6>AUTOPLAZA</h6>
-                      <p><img src={'/iconDashboard.png'} alt={'icon'}/>&nbsp;
-                            Dashboard</p>
-                      <p1>Car Pricing</p1>
-                      <br/><br/>
-                      <p><img src={'/PlusCircle.png'} alt={'icon'}/>&nbsp;Request</p>
-                      <p><img src={'/Union.png'} alt={'icon'}/>&nbsp;Allocation</p>
-                      <p><img src={'/Pricing.png'} alt={'icon'}/>&nbsp;Pricing</p><br/><br/>
-                      <p1>Managment</p1>
-                      <p><img src={'/User.png'} alt={'icon'}/>&nbsp; User </p>
-                      <p><img src={'/Setting.png'} alt={'icon'}/>&nbsp; Setting</p>
+                      <a href="Dashboard"><img src={'/iconDashboard.png'} alt={'icon'}/>&nbsp; Dashboard</a><br/><br/>
+                      <p1>Car Pricing</p1> <br/><br/>
+                      <a href="Request"><img src={'/PlusCircle.png'} alt={'icon'}/>&nbsp;Request</a><br/><br/>
+                      <a href="Allocation"><img src={'/Union.png'} alt={'icon'}/>&nbsp;Allocation</a><br/><br/>
+                      <a href="Pricing"><img src={'/Pricing.png'} alt={'icon'}/>&nbsp;Pricing</a><br/><br/>
+                      <p1>Managment</p1><br/><br/>
+                      <a href="User"><img src={'/User.png'} alt={'icon'}/>&nbsp; User </a><br/><br/>
+                      <a href="Setting"><img src={'/Setting.png'} alt={'icon'}/>&nbsp; Setting</a>
                 </div>
-
                 <div style={{display: 'flex', flexDirection: 'column', width: 'calc(100vw - 300px)', marginTop: 88, padding: 10}}>
                       <div class="Api" style={{display:"flex", justifyContent: 'space-between'}}>
                             <div><span>Pricing</span> &nbsp;<span style={{color:"#B840D6"}}> 169</span></div>
@@ -30,20 +27,21 @@ function App() {
                             <div>Verified &nbsp; <span style={{color:"#909598"}}> 563 </span></div>
                             <div>Archived &nbsp; 563</div>
                       </div>
-                <div style={{display: 'flex', flexDirection: 'column', width: 'calc(100vw - 300px)', marginTop:88, padding:10}}>
-
-                </div>
+                      <div class="admin" style={{display:"flex", justifyContent: 'space-between',}}>
+                            <div><img src={'/noti.png'} alt={'icon'}/></div>
+                            <div><img src={'/member.png'} alt={'icon'}/></div>
+                            <div>Admin</div>
+                      </div>
                       <br/><br/>
                       <div style={{ display: 'flex', justifyContent: 'space-between'}}>
                             <div>
                                   <h1>User List</h1>
                             </div>
                             <div>
-                                  <button className="Add-User"> ADD USER
+                                  <button className="Add-User"> + ADD USER
                                   </button>
                             </div>
                       </div>
-
                       <table id="table">
                             <tr>
                                   <th>Name</th>
